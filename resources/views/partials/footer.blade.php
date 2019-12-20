c48316fbd97ca29beb515ba7661e58608f9dd99f
John Puddephatt <john@jdp.org.uk>
2019-12-20 12:46:45 +0000

    homepage drafted

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="site-footer">
   <div class="container">
-    <h3 class="site-footer--title">Transition by Design</h3>
+    {{-- <h3 class="site-footer--title">Transition by Design</h3> --}}
     @php dynamic_sidebar('sidebar-footer') @endphp
   </div>
 </footer>
