3a093e7dfaab2ed8811d7809cb0f82b48b58e4a1
John Puddephatt <john@jdp.org.uk>
2020-05-05 17:06:18 +0100

    fix home link to manor farm

--- resources/views/partials/home-hero.blade.php
+++ resources/views/partials/home-hero.blade.php
@@ -3,7 +3,7 @@
     <div class="home-hero--text">
       <h2 class="home-hero--title">A contemporary ecological extension of a listed Georgian building.</h2>
       <p class="home-hero--excerpt">At Manor Farm we harmonised natural materials and low energy design within the challenges of an 18th century listed building.</p>
-      <a href="/project/manor-farm/" class="button">See this project&nbsp;&rarr;</a>
+      <a href="/projects/manor-farm/" class="button">See this project&nbsp;&rarr;</a>
     </div>
     <img class="home-hero--image" src="@asset('images/manorfarm2.jpg')" alt="">
   </div>
