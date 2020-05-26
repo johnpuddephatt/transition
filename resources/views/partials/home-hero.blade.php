058c2f6a01dd9c2625a76cb81ddbe927dad4e02b
John Puddephatt <john@jdp.org.uk>
2020-05-27 00:49:54 +0100

    new hero

--- resources/views/partials/home-hero.blade.php
+++ resources/views/partials/home-hero.blade.php
@@ -5,6 +5,11 @@
       <p class="home-hero--excerpt">At Manor Farm we harmonised natural materials and low energy design within the challenges of an 18th century listed building.</p>
       <a href="/projects/manor-farm/" class="button">See this project&nbsp;&rarr;</a>
     </div>
-    <img class="home-hero--image" src="@asset('images/manorfarm2.jpg')" alt="">
+    <div class="home-hero--image">
+      <img src="@asset('images/manorfarm-base.png')" alt="">
+      <div class="image--blob"></div>
+      <div class="image--blob image--blob_2"></div>
+      <div class="image--dots"></div>
+    </div>
   </div>
 </section>
