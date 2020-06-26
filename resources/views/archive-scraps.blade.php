90bc2e6165e04e5f4475a429878c51666fe49666
John Puddephatt <john@jdp.org.uk>
2020-06-26 23:24:20 +0100

    fix textures url

--- resources/views/archive-scraps.blade.php
+++ resources/views/archive-scraps.blade.php
@@ -10,7 +10,6 @@
 
   @include('partials.page-header', ['class' => 'entry-header__section'])
 
-
   <div class="scraps-intro">
     <div class="scraps-intro--text">
       {!! $page_content->post_content !!}
@@ -20,56 +19,57 @@
     </div>
   </div>
 
-  <div class="scraps-grid scraps-grid--wrapper">
+  <div class="scraps-grid--wrapper">
     <div class="scraps-grid">
       @foreach(array_chunk($scraps,4) as $row)
+        @if( $loop->index % 2)
+          <div class="scraps-grid--row">
 
-      @if( $loop->index % 2)
-      <div class="scraps-grid--row">
-        <div class="scraps-grid--column">
-          @if(isset($row[0]))
-          @include('partials.scraps-grid-item', ['scrap' => $row[0]])
+            <div class="scraps-grid--column">
+              @if(isset($row[0]))
+                @include('partials.scraps-grid-item', ['scrap' => $row[0]])
+              @endif
+              @if(isset($row[1]))
+                @include('partials.scraps-grid-item', ['scrap' => $row[1]])
+              @endif
+            </div>
+            <div class="scraps-grid--column">
+              @if(isset($row[2]))
+                @include('partials.scraps-grid-item', ['scrap' => $row[2]])
+              @endif
+            </div>
+          </div>
+
+          @if(isset($row[3]))
+            <div class="scraps-grid--row scraps-grid--row__wide">
+              @include('partials.scraps-grid-item', ['scrap' => $row[3]])
+            </div>
           @endif
-          @if(isset($row[1]))
-          @include('partials.scraps-grid-item', ['scrap' => $row[1]])
+
+
+        @else
+          <div class="scraps-grid--row">
+            <div class="scraps-grid--column">
+              @if(isset($row[0]))
+                @include('partials.scraps-grid-item', ['scrap' => $row[0]])
+              @endif
+            </div>
+            <div class="scraps-grid--column">
+              @if(isset($row[1]))
+                @include('partials.scraps-grid-item', ['scrap' => $row[1]])
+              @endif
+              @if(isset($row[2]))
+                @include('partials.scraps-grid-item', ['scrap' => $row[2]])
+              @endif
+            </div>
+          </div>
+          @if(isset($row[3]))
+            <div class="scraps-grid--row scraps-grid--row__wide">
+              @include('partials.scraps-grid-item', ['scrap' => $row[3]])
+            </div>
           @endif
-        </div>
-        <div class="scraps-grid--column">
-          @if(isset($row[2]))
-          @include('partials.scraps-grid-item', ['scrap' => $row[2]])
-          @endif
-        </div>
-      </div>
-      @if(isset($row[3]))
-      <div class="scraps-grid--row scraps-grid--row__wide">
-        @include('partials.scraps-grid-item', ['scrap' => $row[3]])
-      </div>
-      @endif
-      @else
-      <div class="scraps-grid--row">
-        <div class="scraps-grid--column">
-          @if(isset($row[0]))
-          @include('partials.scraps-grid-item', ['scrap' => $row[0]])
-          @endif
-        </div>
-        <div class="scraps-grid--column">
-          @if(isset($row[1]))
-          @include('partials.scraps-grid-item', ['scrap' => $row[1]])
-          @endif
-          @if(isset($row[2]))
-          @include('partials.scraps-grid-item', ['scrap' => $row[2]])
-          @endif
-        </div>
-      </div>
-      @if(isset($row[3]))
-      <div class="scraps-grid--row scraps-grid--row__wide">
-        @include('partials.scraps-grid-item', ['scrap' => $row[3]])
-      </div>
-      @endif
-      @endif
+        @endif
+      @endforeach
     </div>
-    @endforeach
   </div>
-</div>
-
 @endsection
