1ad888410b97d05820b26fb13727b91afb5c378a
John Puddephatt <john@jdp.org.uk>
2023-04-25 12:02:07 +0100

    load associates

--- app/Controllers/About.php
+++ app/Controllers/About.php
@@ -13,6 +13,8 @@ class About extends Controller
         $this->title = get_the_title();
         $this->thumbnail = get_the_post_thumbnail(null, "large");
 
+        $this->associates = $this->associates();
+
         return $this;
     }
 
@@ -26,6 +28,31 @@ class About extends Controller
                 [
                     "key" => "user_display_settings",
                     "value" => '"display_on_about"',
+                    "compare" => "LIKE",
+                ],
+            ],
+        ]);
+
+        return array_map(function ($member) {
+            $member->image = wp_get_attachment_image(
+                get_field("image", "user_" . $member->ID),
+                "thumbnail"
+            );
+            $member->role = get_field("position", $member->ID);
+            return $member;
+        }, $members);
+    }
+
+    public function associates()
+    {
+        $members = get_users([
+            "exclude" => "1",
+            "orderby" => "meta_value",
+            "meta_key" => "last_name",
+            "meta_query" => [
+                [
+                    "key" => "user_display_settings",
+                    "value" => '"display_on_about_associate"',
                     "compare" => "LIKE",
                 ],
             ],
